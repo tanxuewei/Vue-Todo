dc1c79e211520c79285178c7e9c2ae7a0c713d4d
tanxuewei <tanxuewei94@126.com>
2014-11-21 17:19:41 +0800

    update app.js

--- js/app.js
+++ js/app.js
@@ -3,11 +3,19 @@ require.config({
     paths: {
         'jQuery': 'lib/1.11.1/jquery',
         'underscore': 'lib/1.7.0/underscore',
-        'backbone': 'lib/1.1.2/backbone'
+        'backbone': 'lib/1.1.2/backbone'，
+        'backbone.localStorage': 'lib/backbone.localStorage'
     },
     shim: {
+        'underscore': {
+            exports: '_'
+        },
         'backbone': {
             deps: ['underscore', 'jQuery'],
+            exports: 'Backbone'
+        }，
+        'backbone.localStorage': {
+            deps: ['backbone'],
             exports: 'Backbone'
         }
     }
